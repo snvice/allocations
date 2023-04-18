66eff5ce6e355d1fb8b3b5942ee1b10ec0fe24ae
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 15:28:59 +0300

    Update job.py

--- job.py
+++ job.py
@@ -4,5 +4,8 @@ import streamlit as st
 # Read the CSV file
 df = pd.read_csv('https://raw.githubusercontent.com/snvice/allocations/main/job.csv')
 
+# Add a header
+st.header('Allocations')
+
 # Display the table
 st.write(df)
