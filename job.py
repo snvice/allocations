a9720e3bf57db9cea736748b561c0bde3ccb1349
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 15:23:49 +0300

    Update job.py

--- job.py
+++ job.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Read the CSV file
-df = pd.read_csv('job.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/snvice/allocations/main/job.csv')
 
 # Display the table
 st.write(df)
