6c80a520be17f113afaa470f7b58bb3ba091dabd
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:15:45 +0300

    Update job.py

--- job.py
+++ job.py
@@ -13,10 +13,9 @@ st.markdown("<h1 style='text-align: center; color: #fc6203; font-size: 48px;'>Sc
 
 #########################################################################################################################################################
 
-import time
-import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
+import requests
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -28,9 +27,13 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
-# Display the animation with reduced dimensions
-st_lottie(lottie_hello, speed=1, width=150, height=150, key="hello")
-
+# Display the animation centered on the page
+st.markdown(
+    f"<div style='display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;'>"
+    f"{st_lottie(lottie_hello, speed=1, width=300, height=300, key='hello')}"
+    "</div>",
+    unsafe_allow_html=True,
+)
 
 #########################################################################################################################################################
 
