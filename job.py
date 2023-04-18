61719ad340832ca7cf4be965742bfe438f061537
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:41:03 +0300

    Update job.py

--- job.py
+++ job.py
@@ -8,8 +8,8 @@ import streamlit as st
 
 st.set_page_config(page_title="scanning")
 
-# Set up the title with a larger font size and a custom color
-st.markdown("<h1 style='text-align: center; color: #00FFFF; font-size: 48px;'>Scan Squad</h1>", unsafe_allow_html=True)
+# Set up the title with a larger font size, a custom color, and an underline
+st.markdown("<h1 style='text-align: center; color: #00FFFF; font-size: 48px; text-decoration: underline dotted;'>Scan Squad</h1>", unsafe_allow_html=True)
 
 #########################################################################################################################################################
 
